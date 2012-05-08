e762a63cd13292b14df6d235095430fcbd75896c
jiangwen365 <jiangwen365@gmail.com>
2012-05-08 06:09:45 +0000

    minor

---  pypyodbc/pypyodbc.py
+++  pypyodbc/pypyodbc.py
@@ -12,7 +12,8 @@
 
 #The MIT License (MIT)
 #Copyright (c) 2012 Henry Zhou
-#
+#Copyright (C) 2004 Michele Petrazzo
+
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
 #documentation files (the "Software"), to deal in the Software without restriction, including without 
 #limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
