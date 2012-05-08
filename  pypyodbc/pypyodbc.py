d4dd4084aaadb12ad272550d84ea8c4d572083c4
jiangwen365 <jiangwen365@gmail.com>
2012-05-08 06:06:37 +0000

    add MIT license words

---  pypyodbc/pypyodbc.py
+++  pypyodbc/pypyodbc.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python
 #coding=utf-8
 
-# Copyright (C) 2012 Henry Zhou <jiangwen365@gmail.com>
-
 # PyPyODBC is develped from RealPyODBC 0.1 beta released on 2004 December by Mr. Michele Petrazzo.
 #
 # It's amazing that he implemented basic ODBC funcions with ctypes as earily as year 2004, when
@@ -11,6 +9,18 @@
 # After almost 8 years later, I was thinking of the same idea and found and got amazed by his script.
 # Thanks Michele
 
+
+#The MIT License (MIT)
+#Copyright (c) 2012 Henry Zhou
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+#documentation files (the "Software"), to deal in the Software without restriction, including without 
+#limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+#Software, and to permit persons to whom the Software is furnished to do so, subject to the following #conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO #EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR #THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import sys, os, ctypes, decimal, datetime
 
