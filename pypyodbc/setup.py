25bb8007bbddd88710275aae3c217bf16b85fe9d
jiangwen365 <jiangwen365@gmail.com>
2012-06-23 14:06:06 +0000

    add setup.py

---
+++ pypyodbc/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+setup(
+    name='pypyodbc',
+    version='0.5',
+    description='A Pure Python ctypes ODBC module',
+    author='jiangwen365',
+    author_email='jiangwen365@gmail.com',
+    url='http://http://code.google.com/p/pypyodbc/',
+    py_modules=['pypyodbc'],
+      long_description="""\
+      A Pure Python ctypes ODBC module compatible with PyPy and almost totally same usage as pyodbc
+      """,
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python",
+          "Development Status :: 3 - Alpha",
+      ],
+      keywords='Python, Database, Interface, ODBC, PyPy',
+      license='MIT',
+      install_requires=[
+        'setuptools',
+      ],
+      )
